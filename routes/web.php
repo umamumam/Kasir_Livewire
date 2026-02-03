a6556a8526193ea295f7b1d6764f06801856451e
umamumam <miftahulumam862@gmail.com>
2026-02-03 22:07:54 +0700

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -28,22 +28,23 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Kategori Routes
+    Route::get('/kategori', KategoriController::class)->name('kategori.index');
+    Route::get('/kategori/create', \App\Livewire\CreateKategori::class)->name('kategori.create');
+    Route::get('/kategori/{kategori}/edit', \App\Livewire\EditKategori::class)->name('kategori.edit');
+
+    // Produk Routes
+    Route::get('/produk', ProdukController::class)->name('produk.index');
+    Route::get('/produk/create', \App\Livewire\CreateProduk::class)->name('produk.create');
+    Route::get('/produk/{produk}/edit', \App\Livewire\EditProduk::class)->name('produk.edit');
+    Route::get('/transaksi', TransaksiController::class)->name('transaksi.index');
+    Route::get('/transaksi/create', CreateTransaksi::class)->name('transaksi.create');
+    Route::get('/transaksi/{transaksi}/edit', EditTransaksi::class)->name('transaksi.edit');
+    Route::get('/nota/{transaksiId}', [NotaController::class, 'cetakNota'])->name('transaksi.nota');
+    Route::get('/laporan', [App\Http\Controllers\LaporanController::class, 'index'])->name('laporan.transaksi');
 });
 Route::get('/counter', Counter::class);
 
-// Kategori Routes
-Route::get('/kategori', KategoriController::class)->name('kategori.index');
-Route::get('/kategori/create', \App\Livewire\CreateKategori::class)->name('kategori.create');
-Route::get('/kategori/{kategori}/edit', \App\Livewire\EditKategori::class)->name('kategori.edit');
 
-// Produk Routes
-Route::get('/produk', ProdukController::class)->name('produk.index');
-Route::get('/produk/create', \App\Livewire\CreateProduk::class)->name('produk.create');
-Route::get('/produk/{produk}/edit', \App\Livewire\EditProduk::class)->name('produk.edit');
-Route::get('/transaksi', TransaksiController::class)->name('transaksi.index');
-Route::get('/transaksi/create', CreateTransaksi::class)->name('transaksi.create');
-Route::get('/transaksi/{transaksi}/edit', EditTransaksi::class)->name('transaksi.edit');
-Route::get('/nota/{transaksiId}', [NotaController::class, 'cetakNota'])->name('transaksi.nota');
-Route::get('/laporan', [App\Http\Controllers\LaporanController::class, 'index'])->name('laporan.transaksi');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
